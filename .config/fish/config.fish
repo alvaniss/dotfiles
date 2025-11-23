c2bc389c71ce511e4185c5205b4c930e453affe1
alvaniss <oneweek1001@gmail.com>
2025-11-24 01:06:04 +0600

    update configs

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set fish_greeting ""
-pyenv init - fish | source
-export PATH="/usr/bin:/bin:$PATH"
--- .config/hypr/record_stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pkill -f gpu-screen-recorder
-notify-send "Instant Replay" "Stopped"
-pkill -RTMIN+8 waybar
