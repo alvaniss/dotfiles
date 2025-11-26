d5af2a36999c53995cc79efc02781558944b3730
alvaniss <oneweek1001@gmail.com>
2025-11-27 00:53:58 +0600

    update configs

--- dot_config/hypr/scripts/executable_monitor.sh
+++ dot_config/hypr/scripts/executable_monitor.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 STATE_FILE="$HOME/.config/hypr/scripts/monitor_state"
-MONITOR_NAME="HDMI-A-1"
+CONFIG_FILE="$HOME/.config/hypr/hyprland.conf"
+MONITOR="HDMI-A-1"
 
 [[ -f "$STATE_FILE" ]] || echo "on" > "$STATE_FILE"
 
-CURRENT_STATE=$(cat "$STATE_FILE")
+STATE=$(cat "$STATE_FILE")
 
-if [ "$CURRENT_STATE" = "on" ]; then
-    hyprctl keyword monitor "$MONITOR_NAME, disable"
+if [[ "$STATE" == "on" ]]; then
+    hyprctl keyword monitor "$MONITOR, disable"
+    sed -i "s/^monitor *= *$MONITOR,.*/monitor = $MONITOR, disable/" "$CONFIG_FILE"
     echo "off" > "$STATE_FILE"
+
 else
-    hyprctl keyword monitor "$MONITOR_NAME, 1920x1080@74.97, 0x-580, auto, transform, 3"
+    hyprctl keyword monitor "$MONITOR, 1920x1080@74.97, 0x-580, auto, transform, 3"
+    sed -i "s/^monitor *= *$MONITOR,.*/monitor = $MONITOR, 1920x1080@74.97, 0x-580, auto, transform, 3/" "$CONFIG_FILE"
+    sleep 0.5 && awww img $HOME/.config/hypr/images/desktop-background.png -o $MONITOR -t none
     echo "on" > "$STATE_FILE"
 fi
