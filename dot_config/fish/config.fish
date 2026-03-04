349cca8d2005e7a71634f978090624cdddcf3132
alvaniss <alvaniss1g@gmail.com>
2026-03-04 18:08:24 +0600

    update configs

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,9 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
 end
 
 set fish_greeting ""
-pyenv init - fish | source
-export PATH="/usr/bin:/bin:$PATH"
-
 fish_config theme choose matugen
